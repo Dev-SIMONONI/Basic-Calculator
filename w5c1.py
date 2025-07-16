7fa36c7903478162cb6cb221f635bdb2a59fcc8f
Dev-SIMONONI <bankislimbo18@gmail.com>
2025-07-16 11:14:58 +0100

    intial work

---
+++ w5c1.py
@@ -0,0 +1,18 @@
+'''
+isMarried=True
+isMarried=False
+print(isMarried)
+'''
+
+football="i love it "
+print(bool(football))
+
+cricket="am not a fan"
+print(bool(cricket))
+
+cooking=""
+print(bool(cooking))
+
+jogging=None
+print(bool(jogging))
+
